b815a1cd0788d0ee4dd57eca7e14d8258c1bfa59
vilgax <ken.vilgax@gmail.com>
2024-07-22 22:30:14 +0300

    keystore udate and version update t0 43001

--- app/build/generated/source/buildConfig/debug/com/moodle/golearn/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/moodle/golearn/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.moodle.golearn";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 43000;
+  public static final int VERSION_CODE = 43001;
   public static final String VERSION_NAME = "4.3.0";
 }
